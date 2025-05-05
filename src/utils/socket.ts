7c3cd0771ded141cb9d6c2641d0b42f9f7c5c5b6
mashahir0 <mashahirp@gmail.com>
2025-05-05 12:56:19 +0530

    socket

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,6 +2,6 @@
 import { io } from "socket.io-client";
 
 // ✅ Connect to your backend server (Replace with your backend URL)
-const socket = io("http://localhost:3000"); // Change this in production
+const socket = io(`${import.meta.env.VITE_BACKENDURL}`); // Change this in production
 
 export default socket;
